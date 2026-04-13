0fa4eb1aba475c1da40620e99401624a5116a3e2
yuvrajksinha <yuvraj.k.sinha@gmail.com>
2026-04-13 23:39:54 +0530

    Add Swagger UI and deployment configuration

---
+++ src/main/java/com/javaSpringProject/BankingService/Config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.javaSpringProject.BankingService.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Banking API")
+                        .version("1.0")
+                        .description("REST APIs for banking operations"));
+    }
+}
